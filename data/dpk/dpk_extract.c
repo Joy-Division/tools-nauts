db551d8b0c446893a1feb8a2ed9570c59eeb4ead
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-10-15 12:16:54 -0400

    update dpk_extract.c

--- data/dpk/dpk_extract.c
+++ data/dpk/dpk_extract.c
@@ -9,21 +9,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#ifdef _WIN32
-	#include <direct.h>
-	#define MkDir(dirname)  mkdir(dirname)
-#else
-	#include <sys/stat.h>
-	#include <sys/types.h>
-	#define MkDir(dirname)  mkdir(dirname, 0777)
-#endif
-
 #define PATH_CHAR_LIMIT 260
 
 /*---------------------------------------------------------------------------*/
 
 #include "cmtypes.h"
 #include "cmswap.h"
+#include "cmutil.h"
 
 #include "dpk_extract.h"
 #include "fmt_data_dpk.h"
@@ -45,12 +37,10 @@ void DPK_LoadHeader( FILE *dpk )
 
 void DPK_ReverseHeader( void )
 {
-	if ( DpkEndianFlag ) {
-		DpkHeader.block_size = CM_ByteSwapU32( DpkHeader.block_size );
-		DpkHeader.file_count = CM_ByteSwapU32( DpkHeader.file_count );
-		DpkHeader.entry_end  = CM_ByteSwapU32( DpkHeader.entry_end );
-		DpkHeader.entry_size = CM_ByteSwapU32( DpkHeader.entry_size );
-	}
+	CM_ByteSwap32R( &DpkHeader.block_size );
+	CM_ByteSwap32R( &DpkHeader.file_count );
+	CM_ByteSwap32R( &DpkHeader.entry_end );
+	CM_ByteSwap32R( &DpkHeader.entry_size );
 }
 
 /*---------------------------------------------------------------------------*
@@ -67,11 +57,9 @@ void DPK_LoadEntry( FILE *dpk, int filenum )
 
 void DPK_ReverseEntry( void )
 {
-	if ( DpkEndianFlag ) {
-		DpkEntry.offset   = CM_ByteSwapU32( DpkEntry.offset );
-		DpkEntry.length   = CM_ByteSwapU32( DpkEntry.length );
-		DpkEntry.checksum = CM_ByteSwapU32( DpkEntry.checksum );
-	}
+	CM_ByteSwap32R( &DpkEntry.offset );
+	CM_ByteSwap32R( &DpkEntry.length );
+	CM_ByteSwap32R( &DpkEntry.checksum );
 }
 
 /*---------------------------------------------------------------------------*
@@ -81,7 +69,7 @@ void DPK_ReverseEntry( void )
 bool DPK_CheckID( void )
 {
 	if (( DpkHeader.formatID != 'DIRF' )  // 'FRID' check (LE)
-	 && ( DpkHeader.formatID != 'FRID' )) // 'DIRF' check (BE)
+	&&  ( DpkHeader.formatID != 'FRID' )) // 'DIRF' check (BE)
 	{
 		printf( "\nInvalid format ID!!\n" );
 		return false;
@@ -92,17 +80,16 @@ bool DPK_CheckID( void )
 
 bool DPK_CheckAttribute( void )
 {
-	if ( DpkHeader.attribute == DPK_ATTR_LE ) {
+	if ( DpkHeader.attribute == DPK_ATTR_LE ){
 		DpkEndianFlag = false;
 		printf( "(Little Endian)\n" );
 		return true;
 	}
-	else if ( DpkHeader.attribute == DPK_ATTR_BE ) {
+	else if ( DpkHeader.attribute == DPK_ATTR_BE ){
 		DpkEndianFlag = true;
 		printf( "(Big Endian)\n" );
 		return true;
-	}
-	else {
+	} else {
 		printf( "\nInvalid endian signature!!\n" );
 		return false;
 	}
@@ -121,8 +108,7 @@ bool DPK_CreateOutputDir( char *dir, char *arg )
 	
 	printf( "\nOutput Directory: %s\n", dir );
 	
-	if ( MkDir( dir ) < 0 )
-	{
+	if ( CM_MakeDir( dir ) < 0 ){
 		printf( "Could not create %s !!\n", dir );
 		return false;
 	}
@@ -134,13 +120,13 @@ bool DPK_ExtractFile( FILE *dpk, FILE *out, char *name, char *dir )
 	snprintf( name, PATH_CHAR_LIMIT, "%s/%s", dir, DpkEntry.name );
 	
 	// error check
-	if ( !(out = fopen( name, "wb" ))) {
+	if ( !(out = fopen( name, "wb" ))){
 		printf( "Could not create %s !!\n", name );
 		fclose( out );
 		return false;
 	}
 	
-	char *buffer = malloc(DpkEntry.length);
+	char *buffer = malloc( DpkEntry.length );
 	
 	fseek( dpk, DpkEntry.offset, SEEK_SET );
 	fread( buffer, DpkEntry.length, 1, dpk );
@@ -162,12 +148,12 @@ int main( int argc, char **argv )
 	char fout_name[ PATH_CHAR_LIMIT ] = "";
 	
 	// error check
-	if ( argc != 2 ) {
+	if ( argc != 2 ){
 		printf( "Wrong number of arguments.\n" );
 		printf( "Use: %s example.dpk\n", argv[0] );
 		goto cleanup_exit;
 	}
-	if ( !(fin = fopen( argv[1], "rb" ))) {
+	if ( !(fin = fopen( argv[1], "rb" ))){
 		printf( "Could not open %s !!\n", argv[1] );
 		goto cleanup_exit;
 	}
@@ -177,7 +163,7 @@ int main( int argc, char **argv )
 	if ( !argv1_ext ) goto ext_error;
 	
 	if ( ( strcmp( argv1_ext, ".dpk" ) )
-	&&   ( strcmp( argv1_ext, ".DPK" ) ) ) {
+	&&   ( strcmp( argv1_ext, ".DPK" ) ) ){
 ext_error:
 		printf( "The filename must end with \".dpk\" or \".DPK\"\n" );
 		goto cleanup_fclose;
@@ -189,7 +175,8 @@ ext_error:
 	if ( !DPK_CheckID() || !DPK_CheckAttribute() )
 		goto cleanup_fclose;
 	
-	DPK_ReverseHeader();
+	if ( DpkEndianFlag )
+		DPK_ReverseHeader();
 	
 //	printf( "Format ID  : %.4s\n", (char *)&DpkHeader.formatID );
 	printf( "Attribute  : %08x\n", DpkHeader.attribute );
@@ -198,7 +185,7 @@ ext_error:
 	printf( "Entry End  : %08x\n", DpkHeader.entry_end );
 	printf( "Entry Size : %08x\n", DpkHeader.entry_size );
 	
-	char *outdir = malloc(strlen(argv[1])+1); // +1 for NULL char
+	char *outdir = malloc( strlen(argv[1])+1 ); // +1 for NULL char
 	
 	if ( !DPK_CreateOutputDir( outdir, argv[1] ) )
 		goto cleanup_free;
@@ -209,13 +196,15 @@ ext_error:
 	for ( int i=0 ; i < DpkHeader.file_count ; i++ )
 	{
 		DPK_LoadEntry( fin, i );
-		DPK_ReverseEntry();
+		
+		if ( DpkEndianFlag )
+			DPK_ReverseEntry();
 		
 		printf( "%-12s %08x %08x %08x\n",
-		        DpkEntry.name,
-		        DpkEntry.offset,
-		        DpkEntry.length,
-		        DpkEntry.checksum );
+			DpkEntry.name,
+			DpkEntry.offset,
+			DpkEntry.length,
+			DpkEntry.checksum );
 		
 		if ( !DPK_ExtractFile( fin, fout, fout_name, outdir ) )
 			goto cleanup_free;
