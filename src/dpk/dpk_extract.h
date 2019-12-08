633b4a6a9ffb6e3dcea1736405ba14703b40ba1f
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-12-07 22:21:44 -0500

    formatting

--- src/dpk/dpk_extract.h
+++ src/dpk/dpk_extract.h
@@ -9,25 +9,25 @@
 #include "cmtypes.h"
 #include "fmt_data.h"
 
-/*---------------------------------------------------------------------------*
- * DPK Header
- *---------------------------------------------------------------------------*/
-extern void DPK_LoadHeader    ( DPK_HEADER *head, FILE *dpk );
-extern void DPK_ReverseHeader ( DPK_HEADER *head );
-extern int  DPK_CheckHeader   ( DPK_HEADER *head );
+/*---------------------------------------------------------------------------*/
+// DPK Header
+/*---------------------------------------------------------------------------*/
+void DPK_LoadHeader    ( DPK_HEADER *head, FILE *dpk );
+void DPK_ReverseHeader ( DPK_HEADER *head );
+int  DPK_CheckHeader   ( DPK_HEADER *head );
 
-/*---------------------------------------------------------------------------*
- * DPK Entry Table
- *---------------------------------------------------------------------------*/
-extern void DPK_LoadEntryTable    ( DPK_ENTRY *table, DPK_HEADER *head, FILE *dpk );
-extern void DPK_ReverseEntryTable ( DPK_ENTRY *table, DPK_HEADER *head );
+/*---------------------------------------------------------------------------*/
+// DPK Entry Table
+/*---------------------------------------------------------------------------*/
+void DPK_LoadEntryTable    ( DPK_ENTRY *table, DPK_HEADER *head, FILE *dpk );
+void DPK_ReverseEntryTable ( DPK_ENTRY *table, DPK_HEADER *head );
 
-/*---------------------------------------------------------------------------*
- * DPK File Extract
- *---------------------------------------------------------------------------*/
-extern int DPK_CreateOutputDir( char *dir, char *arg );
+/*---------------------------------------------------------------------------*/
+// DPK File Extract
+/*---------------------------------------------------------------------------*/
+int DPK_CreateOutputDir( char *dir, char *arg );
 
-extern int DPK_ExtractFile(
+int DPK_ExtractFile(
 DPK_ENTRY   *table, /* DPK entry table */
 u_int       index,  /* DPK entry index */
 FILE        *dpk,   /* DPK file ptr    */
